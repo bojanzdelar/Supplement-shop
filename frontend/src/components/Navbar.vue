3ab40a8f3fddf758e39facfc6d43558b77f4940c
Bojan Zdelar <bojan.zdelar@gmail.com>
2021-06-23 16:21:36 +0200

    Added navigation component for mobile devices and updated requirements

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,24 +1,33 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <NavbarMenu :categories="categories" />
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark pt-4 pb-3">
       <div class="container">
-        <router-link to="/" class="navbar-brand">Musclepharm</router-link>
-        <button
+        <div
           class="navbar-toggler"
           type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#navbarMenu"
+          aria-controls="navbarMenu"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <i class="bi bi-list"></i>
+          <!-- <span class="navbar-toggler-icon"></span> -->
+        </div>
+        <router-link to="/" class="navbar-brand">
+          <img
+            src="../assets/images/logo/MPLogo.svg"
+            width="100"
+            height="50"
+            class="d-inline-block align-text-top"
+          />
+        </router-link>
+        <div class="collapse navbar-collapse" id="navbar">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item dropdown">
               <a
-                class="nav-link dropdown-toggle"
+                class="nav-link dropdown-toggle text-uppercase"
                 href="#"
                 id="shopDropdown"
                 role="button"
@@ -46,81 +55,86 @@
                 </li>
               </ul>
             </li>
-            <li class="nav-item">
+            <li class="nav-item text-uppercase">
               <router-link to="/about" class="nav-link">About</router-link>
             </li>
-            <li class="nav-item">
+            <li class="nav-item text-uppercase">
               <router-link to="/contact" class="nav-link">Contact</router-link>
             </li>
           </ul>
-          <ul class="navbar-nav mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a
-                href="#"
-                class="nav-link"
-                data-bs-toggle="offcanvas"
-                data-bs-target="#searchBar"
-                aria-controls="searchBar"
-              >
-                <i class="bi bi-search"></i>
-              </a>
-            </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="settingsDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                <i class="bi bi-gear"></i>
-              </a>
-              <ul class="dropdown-menu" aria-labelledby="settingsDropdown">
-                <li v-if="logged" @click="logout" class="dropdown-item">
-                  Logout
-                </li>
-                <div v-else>
-                  <li>
-                    <div class="d-grid">
-                      <router-link
-                        to="/login"
-                        tag="button"
-                        class="btn btn-success"
-                      >
-                        Login
-                      </router-link>
-                    </div>
-                  </li>
-                  <li>
-                    <router-link to="/register" class="dropdown-item">
-                      New user? <u>Register now</u>
-                    </router-link>
-                  </li>
-                </div>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a
-                href="#"
-                class="nav-link"
-                data-bs-toggle="offcanvas"
-                data-bs-target="#cartBar"
-                aria-controls="cartBar"
-              >
-                <i class="bi bi-cart"></i>
-              </a>
-            </li>
-          </ul>
         </div>
+        <ul class="navbar-nav list-group-horizontal mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a
+              href="#"
+              class="nav-link"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#searchBar"
+              aria-controls="searchBar"
+            >
+              <i class="bi bi-search"></i>
+            </a>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="settingsDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <i class="bi bi-gear"></i>
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="settingsDropdown">
+              <li v-if="logged" @click="logout" class="dropdown-item">
+                Logout
+              </li>
+              <div v-else>
+                <li>
+                  <div class="d-grid">
+                    <router-link
+                      to="/login"
+                      tag="button"
+                      class="btn btn-success"
+                    >
+                      Login
+                    </router-link>
+                  </div>
+                </li>
+                <li>
+                  <router-link to="/register" class="dropdown-item">
+                    New user? <u>Register now</u>
+                  </router-link>
+                </li>
+              </div>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a
+              href="#"
+              class="nav-link"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#cartBar"
+              aria-controls="cartBar"
+            >
+              <i class="bi bi-cart"></i>
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   </div>
 </template>
 
 <script>
+import NavbarMenu from "@/components/NavbarMenu.vue";
+
 export default {
   name: "Navbar",
+  components: {
+    NavbarMenu,
+  },
   data() {
     return {
       categories: [],
@@ -151,3 +165,12 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+@media (max-width: 992px) {
+  .navbar-nav.list-group-horizontal .nav-item {
+    padding-left: 0.5rem;
+    padding-right: 0.5rem;
+  }
+}
+</style>
---
+++ frontend/src/components/NavbarMenu.vue
@@ -0,0 +1,96 @@
+<template>
+  <div
+    class="offcanvas offcanvas-start"
+    tabindex="-1"
+    id="navbarMenu"
+    aria-labelledby="navbarMenu"
+  >
+    <div class="offcanvas-header">
+      <h5 class="offcanvas-title" id="navbarMenuLabel">Close menu</h5>
+      <button
+        type="button"
+        class="btn-close text-reset"
+        data-bs-dismiss="offcanvas"
+        aria-label="Close"
+      ></button>
+    </div>
+    <div class="offcanvas-body">
+      <ul class="list-group list-group-root list-unstyled">
+        <li>
+          <div
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseCategories"
+            class="list-group-item list-group-item-action text-uppercase"
+          >
+            Shop
+          </div>
+          <ul class="collapse list-group list-unstyled" id="collapseCategories">
+            <li>
+              <router-link
+                to="/category/all"
+                data-bs-dismiss="offcanvas"
+                class="
+                  list-group-item list-group-item-action
+                  bg-light
+                  text-uppercase
+                "
+                >All Products</router-link
+              >
+            </li>
+
+            <li v-for="category in categories" :key="category.id">
+              <router-link
+                :to="{
+                  name: 'Category',
+                  params: { id: category.id },
+                }"
+                data-bs-dismiss="offcanvas"
+                class="
+                  list-group-item list-group-item-action
+                  bg-light
+                  text-uppercase
+                "
+              >
+                {{ category.name }}
+              </router-link>
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          <router-link
+            to="/about"
+            data-bs-dismiss="offcanvas"
+            class="list-group-item list-group-item-action text-uppercase"
+          >
+            About
+          </router-link>
+        </li>
+
+        <li>
+          <router-link
+            to="/about"
+            class="list-group-item list-group-item-action text-uppercase"
+          >
+            Contact
+          </router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavbarMenu",
+  props: {
+    categories: Array,
+  },
+};
+</script>
+
+<style scoped>
+.list-group.list-group-root .list-group .list-group-item {
+  padding-left: 30px;
+}
+</style>
